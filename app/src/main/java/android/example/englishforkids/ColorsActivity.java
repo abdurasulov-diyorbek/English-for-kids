fcd160c8b3fc021049d71c9486803aba5d217cf2
Abdurasulov Diyorbek <bossking0704@gmail.com>
2022-04-26 18:15:18 +0500

    Updated

--- app/src/main/java/android/example/englishforkids/ColorsActivity.java
+++ app/src/main/java/android/example/englishforkids/ColorsActivity.java
@@ -3,6 +3,9 @@ package android.example.englishforkids;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("Red", "qizil"));
+        words.add(new Word("Yellow", "sariq"));
+        words.add(new Word("green", "yashil"));
+        words.add(new Word("brown", "jigarrang")) ;
+        words.add(new Word("gray", "gray"));
+        words.add(new Word("black", "qora"));
+        words.add(new Word("white", "oq"));
+        words.add(new Word("orange", "sabzirang"));
+        words.add(new Word("purple", "pushti"));
+
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/android/example/englishforkids/PhrasesActivity.java
+++ app/src/main/java/android/example/englishforkids/PhrasesActivity.java
@@ -3,6 +3,9 @@ package android.example.englishforkids;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("Red", "qizil"));
+        words.add(new Word("Yellow", "sariq"));
+        words.add(new Word("green", "yashil"));
+        words.add(new Word("brown", "jigarrang")) ;
+        words.add(new Word("gray", "gray"));
+        words.add(new Word("black", "qora"));
+        words.add(new Word("white", "oq"));
+        words.add(new Word("orange", "sabzirang"));
+        words.add(new Word("purple", "pushti"));
+
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
     }
 }
